ace9e08047d6d12a9462afdfa66c4bd351410d5b
WelderBm <welderb40@gmail.com>
2026-03-25 20:32:49 -0400

    novimento da torre e esboço da rainha

--- xadrex.c
+++ xadrex.c
@@ -15,5 +15,15 @@ int main() {
     printf("acima\n");
     printf("direita\n");
 
+    //movimento da torre
+    // movimento = printf(acima) LOOP X 2
+    for (int i = 0; i < 2; i++) {
+        printf("acima\n");
+    }
+
+    //movimento da rainha
+    // todos os movimentos: cavalo, bispo e torre
+
+
     return 0;
 }
