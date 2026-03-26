d4376ef98af74f13cfd528bf7b3ecf7344548e3f
WelderBm <welderb40@gmail.com>
2026-03-25 20:11:57 -0400

    first commit

---
+++ xadrex.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // movimento do cavalo
+    // movimento = printf(acima) LOOP X 2
+    // segundomMovimento = printf(direita)
+    for (int i = 0; i < 2; i++) {
+        printf("acima\n");
+    }
+    printf("direita\n");
+
+    return 0;
+}
