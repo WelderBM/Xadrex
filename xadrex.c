70e6d30b06f4db2c2a29aa5034f53e74cd69011f
WelderBm <welderb40@gmail.com>
2026-03-25 21:09:23 -0400

    feat: adição de funções recursivas e aplicando reutilização das mesmas

--- xadrex.c
+++ xadrex.c
@@ -1,29 +1,61 @@
 #include <stdio.h>
 
-int main() {
-    // movimento do cavalo
-    // movimento = printf(acima) LOOP X 2
-    // segundomMovimento = printf(direita)
-    for (int i = 0; i < 2; i++) {
+// Implemente funções recursivas para simular o movimento de cada peça, substituindo os loops originais
+
+// Função recursiva para o movimento do cavalo (DEVE SER EM L)
+
+// Função recursiva para o movimento do bispo
+void movimentoCima(int movimentos)
+{
+    if (movimentos > 0)
+    {
         printf("acima\n");
+        movimentoCima(movimentos - 1);
     }
-    printf("direita\n");
-
-    //movimento do bispo
-    // movimento = printf(acima)
-    // segundomMovimento = printf(direita)
-    printf("acima\n");
-    printf("direita\n");
-
-    //movimento da torre
-    // movimento = printf(acima) LOOP X 2
-    for (int i = 0; i < 2; i++) {
-        printf("acima\n");
+}
+void movimentoDireita(int movimentos)
+{
+    if (movimentos > 0)
+    {
+        printf("direita\n");
+        movimentoDireita(movimentos - 1);
     }
+}
 
-    //movimento da rainha
-    // todos os movimentos: cavalo, bispo e torre
+void movimentoCavalo()
+{
+    movimentoCima(2);
+    movimentoDireita(1);
+}
 
+void movimentoBispo(int movimentos)
+{
+    if (movimentos > 0)
+    {
+        movimentoCima(1);
+        movimentoDireita(1);
+        movimentoBispo(movimentos - 1);
+    }
+}
+
+void movimentoTorre(int movimentos)
+{
+    if (movimentos > 0)
+    {
+        movimentoCima(1);
+        movimentoTorre(movimentos - 1);
+    }
+}
+
+int main()
+{
+    // entrada de dados
+    printf("Movimento do cavalo:\n");
+    movimentoCavalo();
+    printf("Movimento do bispo:\n");
+    movimentoBispo(5);
+    printf("Movimento da torre:\n");
+    movimentoTorre(7);
 
     return 0;
 }
